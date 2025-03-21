b7322c316aec69ff900855944893240f6d0c3c32
Denis Demenkov <denis.demenkovg@gmail.com>
2025-03-20 22:38:41 -0500

    Update llm.py to have config variables and logging

--- src/llm.py
+++ src/llm.py
@@ -1,6 +1,10 @@
+import logging
+
 from openai import OpenAI
 
 from configs import config
+
+logger = logging.getLogger(__name__)
 
 class LlamaService:
     def __init__(self):
@@ -22,14 +26,16 @@ class LlamaService:
             messages.append({"role": "user", "content": prompt})
             
             response = self.client.chat.completions.create(
-                model="some_llama",
+                model=config.model_name,
                 messages=messages,
                 temperature=0.1,
                 max_tokens=256,
                 top_p=0.95
             )
-            
-            return response.choices[0].message.content.strip()
-                
+            response = response.choices[0].message.content.strip()
+
         except Exception as e:
-            return f"Ошибка при обращении к API: {str(e)}" 
+            logger.exception(f'Ошибка при обращении к API: {str(e)}')
+            response = 'Извините, что-то пошло не так. Попробуйте еще раз!'
+
+        return response
